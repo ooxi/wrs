ae4dd94224d2ee915e162250a9ada00d87d3579c
ooxi <violetland@mail.ru>
2012-12-29 14:49:03 +0100

    Development...

--- ship.js
+++ ship.js
@@ -3,7 +3,16 @@
 
 module.exports = function(name) {
 
+	/* Position which the ship tries to achieve
+	 */
+	var desired_position = {
+		x: 0.0,
+		y: 0.0
+	};
 
+	/* Is this ship still alive?
+	 */
+	var is_alive = true;
 
 };
 
