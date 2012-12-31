a44ebe37b1b3b07572da5b427d2e767d404080f6
ooxi <violetland@mail.ru>
2012-12-31 06:03:51 +0100

    Development...

--- server/http.js
+++ server/http.js
@@ -60,44 +60,55 @@ module.exports = function(_game) {
 
 
 
-		/* Frequently used commands have to be checked first
+		/* Do not terminate server on exceptions
 		 */
-		if ('/radar' === action.pathname) {
-//			wrs.http.radar(_game, response);
-		} else if ('/move' === action.pathname) {
-//			move(action.query, send);
-		} else if ('/shoot' === action.pathname) {
-//			shoot(action.query, send);
+		try {
 
-		/* Dump must not be called by AIs but is frequently used by
-		 * GUIs
-		 */
-		} else if ('/dump' === action.pathname) {
-//			dump(action.query, send);
+			/* Frequently used commands have to be checked first
+			 */
+			if ('/radar' === action.pathname) {
+//				wrs.http.radar(_game, response);
+			} else if ('/move' === action.pathname) {
+//				move(action.query, send);
+			} else if ('/shoot' === action.pathname) {
+//				shoot(action.query, send);
 
-		/* Other commands will only occasionally be invoked, order of
-		 * check does not matter
-		 */
-		} else if ('/configuration' === action.pathname) {
-//			send(200, configuration);
-		} else if ('/connect' === action.pathname) {
-			wrs.http.connect(_game, response);
-		} else if ('/is-alive' === action.pathname) {
-//			is_alive(action.query, send);
-		} else if ('/suicide' === action.pathname) {
-//			suicide(action.query, send);
-		} else if ('/team' === action.pathname) {
-			wrs.http.team(_game, response);
+			/* Dump must not be called by AIs but is frequently used by
+			 * GUIs
+			 */
+			} else if ('/dump' === action.pathname) {
+//				dump(action.query, send);
 
-		/* Not a real command, just there for convenience
-		 */
-		} else if ('/' === action.pathname) {
-//			gui(response);
+			/* Other commands will only occasionally be invoked, order of
+			 * check does not matter
+			 */
+			} else if ('/configuration' === action.pathname) {
+//				send(200, configuration);
+			} else if ('/connect' === action.pathname) {
+				wrs.http.connect(_game, response);
+			} else if ('/is-alive' === action.pathname) {
+//				is_alive(action.query, send);
+			} else if ('/suicide' === action.pathname) {
+//				suicide(action.query, send);
+			} else if ('/team' === action.pathname) {
+				wrs.http.team(_game, response);
 
-		/* Unknown command
+			/* Not a real command, just there for convenience
+			 */
+			} else if ('/' === action.pathname) {
+//				gui(response);
+
+			/* Unknown command
+			 */
+			} else {
+				response.error(404, 'Unknown method');
+			}
+
+		/* Print exception on console and forward to client
 		 */
-		} else {
-			response.error(404, 'Unknown method');
+		} catch (exception) {
+			console.log('Unexpected exception while serving http request: %j', exception);
+			response.error(500, exception);
 		}
 	};
 
