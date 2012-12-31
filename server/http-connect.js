c372e9ac93f389c44de702b5951dcc6e36d76ad4
ooxi <violetland@mail.ru>
2012-12-31 04:57:22 +0100

    Development...

--- server/http-connect.js
+++ server/http-connect.js
@@ -31,7 +31,7 @@
  * Adds a new ship, if possible
  */
 module.exports = function(configuration, orbit, response) {
-	if (!response.require([])) {
+	if (!response.require(['name', 'team'])) {
 		return;
 	}
 };
