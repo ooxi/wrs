12ab8c6e831591ace23fc11a19b6868fd9221a38
ooxi <violetland@mail.ru>
2012-12-31 16:32:16 +0100

    Development...

--- client/ai-fly-to.js
+++ client/ai-fly-to.js
@@ -23,6 +23,10 @@
  */
 'use strict';
 
+var wrs = {
+	point:	require('./point.js')
+};
+
 
 
 
@@ -31,7 +35,65 @@
  * Flys a ship to a position without trying to avoid anything. Will usually be
  * used by other AIs
  */
-module.exports = function() {
+module.exports = function(_api, _configuration, _radar, _ship) {
+
+	/* Position which the ship tries to achieve
+	 */
+	var _desired_position = new wrs.point(0.0, 0.0);
+	var _desired_distance = 100.0;
+	var _desired_cb = null;
+
+
+
+	/**
+	 * Sets the desired position
+	 */
+	this.fly_to = function(x, y, cb) {
+		_desired_position.x = x;
+		_desired_position.y = y;
+
+		if ('function' === typeof(cb)) {
+			_desired_cb = cb;
+		} else {
+			_desired_cb = null;
+		}
+	};
+
+
+
+	/**
+	 * Tries to fly to the desired position without avoiding shots
+	 */
+	this.move = function() {
+		var position = _radar.ship(_ship);
+		if (null === self) {
+			console.log('[ai-fly-to] I don\'t know myself');
+			return;
+		}
+
+		var direction = util.look_at(
+			self,
+			_desired_position
+		);
+
+		_api.move(
+			_ship.private_key(),
+			direction.x * _configuration['max-ship-speed'],
+			direction.y * _configuration['max-ship-speed']
+		);
+
+		/* If ship is near the desired position, than the callback will
+		 * be invoked once
+		 */
+		if ('function' === typeof(_desired_cb)) {
+			if (util.distance_sqr(this.position(), _desired_position) < _desired_distance * _desired_distance) {
+				var cb = _desired_cb;
+				_desired_cb = null;
+				cb();
+			}
+		}
+	};
+
 };
 
 
