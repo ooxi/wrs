3f36facd0efeef0ea8cde7062529646cfd2326ce
ooxi <violetland@mail.ru>
2012-12-31 05:34:58 +0100

    Development...

--- server/http.js
+++ server/http.js
@@ -62,39 +62,39 @@ module.exports = function(_game) {
 		/* Frequently used commands have to be checked first
 		 */
 		if ('/radar' === action.pathname) {
-			wrs.http.radar(_game, response);
+//			wrs.http.radar(_game, response);
 		} else if ('/move' === action.pathname) {
-			move(action.query, send);
+//			move(action.query, send);
 		} else if ('/shoot' === action.pathname) {
-			shoot(action.query, send);
+//			shoot(action.query, send);
 
 		/* Dump must not be called by AIs but is frequently used by
 		 * GUIs
 		 */
 		} else if ('/dump' === action.pathname) {
-			dump(action.query, send);
+//			dump(action.query, send);
 
 		/* Other commands will only occasionally be invoked, order of
 		 * check does not matter
 		 */
 		} else if ('/configuration' === action.pathname) {
-			send(200, configuration);
+//			send(200, configuration);
 		} else if ('/connect' === action.pathname) {
 			wrs.http.connect(_game, response);
 		} else if ('/is-alive' === action.pathname) {
-			is_alive(action.query, send);
+//			is_alive(action.query, send);
 		} else if ('/suicide' === action.pathname) {
-			suicide(action.query, send);
+//			suicide(action.query, send);
 
 		/* Not a real command, just there for convenience
 		 */
 		} else if ('/' === action.pathname) {
-			gui(response);
+//			gui(response);
 
 		/* Unknown command
 		 */
 		} else {
-			send(404, 'Unknown method');
+			response.error(404, 'Unknown method');
 		}
 	};
 
