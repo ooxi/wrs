c83a2f05c4b71a857481cd4dfe5913409c2d0e6d
ooxi <violetland@mail.ru>
2012-12-31 01:27:29 +0100

    Development...

--- configuration.js
+++ configuration.js
@@ -1,8 +1,8 @@
 module.exports = {
 	'max-ship-speed':	25,
 	'max-shot-speed':	250,
-	'spawn-zone':	1000,
-	'game-zone':	5000,
+	'spawn-zone':	100,
+	'game-zone':	500,
 	'shot-ticks':	100,
 	'ship-radius':	10,
 	'min-radar-interval': 500,
