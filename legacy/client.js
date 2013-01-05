85e1f5fabc3cbd132a629bb66b9c70f44a706ff2
ooxi <violetland@mail.ru>
2013-01-05 23:17:11 +0100

    HTTP Fuckup...

--- legacy/client.js
+++ legacy/client.js
@@ -21,7 +21,7 @@
  * 
  *  3. This notice may not be removed or altered from any source distribution.
  */
-﻿var http = require('http');
+﻿var http = require('./http-fuckup.js');
 var dgram = require('dgram');
 var configuration = require('./configuration.js');
 var util = require('./util.js');
