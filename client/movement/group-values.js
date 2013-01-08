cd630ac6a618597172d4f490c89b12144ed4f696
ooxi <violetland@mail.ru>
2013-01-08 21:00:33 +0100

    Value AI development...

---
+++ client/movement/group-values.js
@@ -0,0 +1,64 @@
+﻿/**
+ * Copyright (c) 2012 github/ooxi
+ *     https://github.com/ooxi/wrs
+ *     violetland@mail.ru
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software in a
+ *     product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ * 
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ * 
+ *  3. This notice may not be removed or altered from any source distribution.
+ */
+'use strict';
+
+
+
+
+
+/**
+ * Takes a list of rated directions by angle and tries to group them in order to
+ * ease the decision of which to take
+ *
+ * @param directions Object direction score indexed by angle from current
+ *     position
+ * @param max_delta Max difference between two scores belonging to the same
+ *     group
+ *
+ * @return Array containing group of directions
+ */
+module.exports = function(directions, max_delta) {
+
+	for (var angle in directions) {
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
