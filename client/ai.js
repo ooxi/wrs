cebc947c91e4ae571932d6b7c0b5739fecf6523d
ooxi <violetland@mail.ru>
2013-01-08 20:54:51 +0100

    Value AI development...

--- client/ai.js
+++ client/ai.js
@@ -172,8 +172,8 @@ module.exports = Class.extend({
 			var value = this.movement.value(ship, try_position);
 
 			this.gui.arrow(try_position, new wrs.point(
-				Math.cos(angle) * value,
-				Math.sin(angle) * value
+				Math.cos(angle) * (5.0 - value) * 100.0,
+				Math.sin(angle) * (5.0 - value) * 100.0
 			), 0.5, 'blue');
 
 			if (value > best_value) {
