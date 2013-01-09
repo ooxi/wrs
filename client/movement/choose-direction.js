9ca8b5291cbcb3c8e3ee142ee2c2492818cb0ba6
ooxi <violetland@mail.ru>
2013-01-09 05:01:55 +0100

    Value AI development...

---
+++ client/movement/choose-direction.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2013 github/ooxi
+ *     https://github.com/ooxi/wrs
+ *     violetland@mail.ru
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software in a
+ *     product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ * 
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ * 
+ *  3. This notice may not be removed or altered from any source distribution.
+ */
+'use strict';
+
+
+
+
+
+/**
+ * Chooses a direction from a group of directions
+ *
+ * @param ship The ship to move
+ * @param position The ship's current position and movement
+ * @param direction_groups Direction groups to choose from
+ */
+module.exports = function(ship, position, direction_groups) {
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
