2acd18a46defa1a2582ced2f9ec14da3773e29fb
ooxi <violetland@mail.ru>
2013-01-09 06:29:20 +0100

    Value AI development...

--- client/movement/choose-direction.js
+++ client/movement/choose-direction.js
@@ -106,12 +106,10 @@ module.exports = function(ship, position, direction_groups) {
 	 * consider that angle, too
 	 */
 	var ship_speed = Math.sqrt(wrs.util.length_sqr(position.dx, position.dy));
-//	var exact_ship_angle = Math.acos(position.dx / ship_speed);
 	var exact_ship_angle = Math.atan2(position.dy, position.dx);
 	if (exact_ship_angle < 0.0) {
 		exact_ship_angle += 2.0 * Math.PI;
 	}
-console.log('ship angle is %j vs %j', exact_ship_angle, Math.acos(position.dx / ship_speed));
 	var approx_ship_angle = null;
 
 	if (null !== exact_ship_angle) {
